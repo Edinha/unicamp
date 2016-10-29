de013f780d8250ce8b47dca9423886a1ef79fcc2
edinha <edinha.gon.cruz@gmail.com>
2016-10-29 21:27:38 -0200

    Implementing basic functions for heap

--- mc-202/lab07/heap.c
+++ mc-202/lab07/heap.c
@@ -9,12 +9,56 @@
 
 Heap * createHeap(int size) {
 	Heap * heap = malloc(sizeof(Heap));
+	heap->maxSize = size;
+	heap->actualSize = ZERO_INIT;
+
 	heap->data = malloc(size * sizeof(Cache));
-	heap->size = 0;
+	for (int i = 0; i < size; i++) {
+		heap->data[i].number = INVALID_NUMBER_INIT;
+		heap->data[i].priority = ZERO_INIT;
+	}
+
 	return heap;
 }
 
+Cache createCache(int number) {
+	Cache cache;
+	cache.number = number;
+	cache.priority = ZERO_INIT;
+	return cache;
+}
+
+int isFull(Heap * heap) {
+	return (heap->actualSize == heap->maxSize);
+}
+
+int left(int position) {
+	return (position * 2) + 1;
+}
+
+int right(int position) {
+	return (position * 2) + 2;
+}
+
+int parent(int position) {
+	return (position - 1) / 2;
+}
+
+int positionExists(Heap * heap, int position) {
+	return (position < heap->actualSize);
+}
+
 void insert(Heap * heap, int cacheElement) {
+	Cache cache = createCache(cacheElement);
+
+	if (isFull(heap)) {
+		// TODO remover o ultimo lugar do cache em prioridade
+	}
+
+	add(heap, cache, 0);
+}
+
+void add(Heap * heap, Cache cache, int position) {
 
 }
 
--- mc-202/lab07/heap.h
+++ mc-202/lab07/heap.h
@@ -13,6 +13,9 @@
  * a ordem de prioridade para o cache do programa
  */
 
+#define ZERO_INIT 0
+#define INVALID_NUMBER_INIT -1
+
 /* Define a estrutura de um dado de cache do heap */
 typedef
 	struct {
@@ -22,15 +25,37 @@ typedef
 
 typedef
 	struct {
-		int size;
 		Cache * data;
+		int maxSize,
+			actualSize;
 	} Heap;
 
 /* Inicializa um ponteiro de heap a partir de um tamanho maximo para o vetor */
 Heap* createHeap(int);
 
+/* Inicializa um elemento de cache a partir de seu numero */
+Cache createCache(int);
+
+/* Retorna verdadeiro caso o tamanho atual seja igual ao maximo */
+int isFull(Heap*);
+
+/* Retorna a esquerda de uma posicao parametrizada */
+int left(int);
+
+/* Retorna a direita de uma posicao parametrizada */
+int right(int);
+
+/* Retorna o pai de uma posicao parametrizada */
+int parent(int);
+
+/* Retorna verdadeiro caso a posicao inteira exista no heap */
+int positionExists(Heap*, int);
+
 /* Insere na heap o elemento, aumentando sua prioridade caso ele esteja contido no heap */
 void insert(Heap*, int);
+
+/* Adiciona recursivamente um elemento de cache ao heap a partir de uma posicao */
+void add(Heap*, Cache, int);
 
 /* Aumenta a prioridade de um elemento de cache */
 void higher(Cache*);
