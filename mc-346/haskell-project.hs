22d010f799a6fa9353b586258718c12b97f21150
edinha <edinha.gon.cruz@gmail.com>
2018-05-17 13:46:30 -0300

    MC 346 - Haskell project start of kruskal algorithm

--- mc-346/haskell-project.hs
+++ mc-346/haskell-project.hs
@@ -1,12 +1,23 @@
-data Ling = Ling String Int deriving (Show)
+import Data.List (sort)
+import Data.Map (insert, empty)
 
-naisu (Ling s n) = s
+data Vertex = Vertex String [Float] deriving (Ord, Show)
+data Edge = Edge Float Vertex Vertex deriving (Eq, Ord, Show)
 
-
-data Vertex = String [Float]
-data Edge = Float Vertex Vertex deriving (Ord)
+instance Eq Vertex where (==) (Vertex x _) (Vertex y _) = x == y
 
 distance :: Vertex -> Vertex -> Float
-distance (Vertex _ a) (Vertex _ b) = 
-  sqrt $ sum $ foldl (\acc (x,y) -> (x-y)ˆ2 ) 0 $
+distance (Vertex _ a) (Vertex _ b) =
+  sqrt $ foldl (\acc (x,y) -> acc + (x-y)^2 ) 0 $
   zipWith (\f s -> (f,s) ) a b
+
+edges [] = []
+edges (x:xs) = (foldl (\acc y -> (Edge (distance x y) x y) : acc) [] xs) ++ edges xs
+
+get_edges l = sort $ edges l
+
+-- get_edges [ (Vertex "a" [0, 0]), (Vertex "b" [0, 1]), (Vertex "c" [0, 2]) ]
+
+kruskal _ 0  = Data.Map.empty
+kruskal [] _ = Data.Map.empty
+kruskal ((Edge _ v y):rest) k = Data.Map.insert v 1 $ kruskal rest (k-1)
