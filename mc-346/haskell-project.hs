fcdb02ec059c2770b58af54006a2e2a833394407
edinha <edinha.gon.cruz@gmail.com>
2018-05-18 15:48:36 -0300

    MC 346 - Haskell project unify sets logic fix

--- mc-346/haskell-project.hs
+++ mc-346/haskell-project.hs
@@ -1,5 +1,5 @@
-import Data.List (sort)
-import Data.Map (findWithDefault, insert, adjust, union, empty)
+import Data.List (sort, groupBy, sortBy)
+import Data.Map ((!), findWithDefault, insert, adjust, toList, empty, delete)
 
 data Vertex = Vertex String [Float] String deriving (Ord, Show)
 data Edge = Edge Float Vertex Vertex deriving (Eq, Ord, Show)
@@ -16,16 +16,16 @@ edges (x:xs) = (foldl (\acc y -> (Edge (distance x y) x y) : acc) [] xs) ++ edge
 
 get_edges l = sort $ edges l
 
--- get_edges [ (Vertex "a" [0, 0] "a"), (Vertex "b" [0, 1] "b"), (Vertex "c" [0, 2] "b") ]
+-- get_edges [ (Vertex "a" [0, 0] "a"), (Vertex "b" [0, 1] "b"), (Vertex "c" [0, 2] "c") ]
 
-init_sets [] = empty
-init_sets ((Vertex a b _):rest) = Data.Map.insert a (Vertex a b a) (init_sets rest)
+init_sets l = foldl (\acc (Vertex key _ _) -> insert key [key] acc) empty l
+same_set m (Vertex first _ _) (Vertex second _ _) = (findWithDefault [] first m) == (findWithDefault [] second m)
 
-same_set m (Vertex first _ _) (Vertex second _ _) = a == b
-  where (Vertex _ _ a) = findWithDefault (Vertex "" [] "") first m
-        (Vertex _ _ b) = findWithDefault (Vertex "" [] "") second m
-
-unify_sets m (Vertex _ _ a) (Vertex second p _) = adjust (\ _ -> (Vertex second p a)) second m
+unify_sets m (Vertex first _ _) (Vertex second _ _)
+  | null first_group = delete first $ adjust (\l -> first_group ++ l) second m
+  | otherwise        = delete second $ adjust (\l -> second_group ++ l) first m
+  where first_group = findWithDefault [] first m
+        second_group = findWithDefault [] second m
 
 kruskal _ 0  m = m
 kruskal [] _ m = m
@@ -34,4 +34,6 @@ kruskal ((Edge _ v y):rest) k m
   | otherwise      = kruskal rest (k-1) (unify_sets m v y)
 
 
-minimal_spanning_tree e v k = kruskal e ((length v) - k) (init_sets v)
+minimal_spanning_tree e v k = kruskal e ((length v) - (k - 1)) (init_sets v)
+
+extract_groups l = map (\(_, x) -> x) $ toList l
