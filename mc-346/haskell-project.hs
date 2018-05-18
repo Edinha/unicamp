cb2e710218744a0ba40e28266b3e2e7cfe4caa6b
edinha <edinha.gon.cruz@gmail.com>
2018-05-18 10:36:51 -0300

    MC 346 - Haskell project kruskal seems like its working

--- mc-346/haskell-project.hs
+++ mc-346/haskell-project.hs
@@ -1,13 +1,13 @@
 import Data.List (sort)
-import Data.Map (insert, empty)
+import Data.Map (findWithDefault, insert, adjust, union, empty)
 
-data Vertex = Vertex String [Float] deriving (Ord, Show)
+data Vertex = Vertex String [Float] String deriving (Ord, Show)
 data Edge = Edge Float Vertex Vertex deriving (Eq, Ord, Show)
 
-instance Eq Vertex where (==) (Vertex x _) (Vertex y _) = x == y
+instance Eq Vertex where (==) (Vertex x _ _) (Vertex y _ _) = x == y
 
 distance :: Vertex -> Vertex -> Float
-distance (Vertex _ a) (Vertex _ b) =
+distance (Vertex _ a _) (Vertex _ b _) =
   sqrt $ foldl (\acc (x,y) -> acc + (x-y)^2 ) 0 $
   zipWith (\f s -> (f,s) ) a b
 
@@ -16,8 +16,22 @@ edges (x:xs) = (foldl (\acc y -> (Edge (distance x y) x y) : acc) [] xs) ++ edge
 
 get_edges l = sort $ edges l
 
--- get_edges [ (Vertex "a" [0, 0]), (Vertex "b" [0, 1]), (Vertex "c" [0, 2]) ]
+-- get_edges [ (Vertex "a" [0, 0] "a"), (Vertex "b" [0, 1] "b"), (Vertex "c" [0, 2] "b") ]
 
-kruskal _ 0  = Data.Map.empty
-kruskal [] _ = Data.Map.empty
-kruskal ((Edge _ v y):rest) k = Data.Map.insert v 1 $ kruskal rest (k-1)
+init_sets [] = empty
+init_sets ((Vertex a b _):rest) = Data.Map.insert a (Vertex a b a) (init_sets rest)
+
+same_set m (Vertex first _ _) (Vertex second _ _) = a == b
+  where (Vertex _ _ a) = findWithDefault (Vertex "" [] "") first m
+        (Vertex _ _ b) = findWithDefault (Vertex "" [] "") second m
+
+unify_sets m (Vertex _ _ a) (Vertex second p _) = adjust (\ _ -> (Vertex second p a)) second m
+
+kruskal _ 0  m = m
+kruskal [] _ m = m
+kruskal ((Edge _ v y):rest) k m
+  | same_set m v y = kruskal rest k m
+  | otherwise      = kruskal rest (k-1) (unify_sets m v y)
+
+
+minimal_spanning_tree e v k = kruskal e ((length v) - k) (init_sets v)
