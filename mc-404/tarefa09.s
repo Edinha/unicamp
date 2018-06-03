a7a032a6e09fb89afaf0681f4a3bcdceab8f1524
edinha <edinha.gon.cruz@gmail.com>
2018-06-02 21:21:01 -0300

    MC 404 - Alterations in behavior

--- mc-404/tarefa09.s
+++ mc-404/tarefa09.s
@@ -11,7 +11,15 @@
   b stop_button_press               @ Configura as interupções
 
 stop_button_press:
-  mov r10, #0x66
+  push { r1, lr }
+
+  ldrb r1, on_ride_state
+
+  cmp r1, #ON_TRAVEL                @ Apenas executa caso esteja em trânsito, e não parado
+  ldreq r1, =request_stop_msg
+  bleq write_console                @ Escreve a mensagem de parada solicitada
+
+  pop { r1, lr }
   movs pc, lr
 
 write_console:
@@ -40,23 +48,38 @@ msg_size_loop:
   pop { r0, r1 }
   bx lr
 
+wait_departure_buttons:             @ Esse método espera um clique nos botões de partida e retorna o endereço do clicado
+  ldr r0, =FIRST_STOP_DEPARTURE
+  ldr r1, [r0]
+  cmp r1, #1
+  bxeq lr                           @ Caso seja o primero ponto de chegada
+
+  ldr r0, =SECOND_STOP_DEPARTURE
+  ldr r1, [r0]
+  cmp r1, #1
+  bxeq lr                           @ Caso seja o segundo ponto de chegada
+
+  ldr r0, =THIRD_STOP_DEPARTURE
+  ldr r1, [r0]
+  cmp r1, #1
+  bxeq lr                           @ Caso seja o terceiro ponto de chegada
+
+  b wait_departure_buttons          @ Espera em loop infinito caso não retorne em nenhum dos três casos
+
 wait_arrival_buttons:               @ Esse método espera um clique nos botões de chegada e retorna o endereço do clicado
   ldr r0, =FIRST_STOP_ARRIVAL
   ldr r1, [r0]
   cmp r1, #1
-  ldreq r3, =first_stop_name
   bxeq lr                           @ Caso seja o primero ponto de chegada
 
   ldr r0, =SECOND_STOP_ARRIVAL
   ldr r1, [r0]
   cmp r1, #1
-  ldreq r3, =second_stop_name
   bxeq lr                           @ Caso seja o segundo ponto de chegada
 
   ldr r0, =THIRD_STOP_ARRIVAL
   ldr r1, [r0]
   cmp r1, #1
-  ldreq r3, =third_stop_name
   bxeq lr                           @ Caso seja o terceiro ponto de chegada
 
   b wait_arrival_buttons            @ Espera em loop infinito caso não retorne em nenhum dos três casos
@@ -72,17 +95,43 @@ _start:
 
   mov r0, #0x10
   bic r0, r0, #(IRQ)                @ Habilita interrupções
-  msr cpsr,r0                       @ Move processador para modo usuário
+  msr cpsr, r0                      @ Move processador para modo usuário
   mov sp, #STACK                    @ Inicializa a pilha
 
-main_loop:
-  bl wait_arrival_buttons           @ Espera o clique de um botão de arrival
+  ldr r0, =stop_name_address
+  ldr r1, =first_stop_name
+  str r1, [r0]                      @ Inicializa o nome da parada atual na variável
 
+  ldr r0, =on_ride_state
+  ldr r1, =ON_TRAVEL
+  strb r1, [r0]                     @ Inicializa o estado atual do transporte
+
+main_loop:
   ldr r1, =next_stop_msg
   bl write_console                  @ Escreve próximia parada no console
 
-  mov r1, r3                        @ Copia o valor do nome da parada para r1 e escreve no console
+  ldr r1, stop_name_address
+  bl write_console                  @ Nome da próxima parada
+
+  bl wait_arrival_buttons           @ Espera o clique de um botão de arrival
+
+  ldr r0, =on_ride_state
+  ldr r1, =STOPPED
+  strb r1, [r0]                     @ Muda o estado para parado
+
+  ldr r1, =arrival_msg
+  bl write_console                  @ Mensagem de chegada ao ponto
+
+  ldr r1, stop_name_address
   bl write_console
+
+  bl wait_departure_buttons        @ Espera o clique de um botão de departure
+
+  ldr r0, =on_ride_state
+  ldr r1, =ON_TRAVEL
+  strb r1, [r0]                     @ Muda o estado para andando novamente
+
+  b main_loop                       @ Continua em loop eterno
 
 end:
   mov r10, #0x66
@@ -99,21 +148,22 @@ next_stop_msg:                      @ "Proxima parada "
   .byte 0x50, 0x72, 0x6F, 0x78, 0x69, 0x6D, 0x61, 0x20, 0x70, 0x61, 0x72, 0x61, 0x64, 0x61, 0x20, 0x0
 
 request_stop_msg:                   @ "Parada solicitada "
-  .byte 0x50, 0x61, 0x72, 0x61, 0x64, 0x61, 0x20, 0x73, 0x6F, 0x6C, 0x69, 0x6C, 0x63, 0x74, 0x61, 0x64, 0x61, 0x0
+  .byte 0x50, 0x61, 0x72, 0x61, 0x64, 0x61, 0x20, 0x73, 0x6F, 0x6C, 0x69, 0x6C, 0x63, 0x74, 0x61, 0x64, 0x61, 0x0A, 0x0
 
 arrival_msg:                        @ "Chegamos a "
   .byte 0x43, 0x68, 0x65, 0x67, 0x61, 0x6D, 0x6F, 0x73, 0x20, 0x61, 0x20, 0x0
 
 first_stop_name:
-  .byte 0x55, 0x6E, 0x69, 0x0
+  .byte 0x55, 0x6E, 0x69, 0x0A, 0x0
 
 second_stop_name:
-  .byte 0x42, 0x61, 0x72, 0x0
+  .byte 0x42, 0x61, 0x72, 0x0A, 0x0
 
 third_stop_name:
-  .byte 0x43, 0x65, 0x6E, 0x0
+  .byte 0x43, 0x65, 0x6E, 0x0A, 0x0
 
 on_ride_state: .skip 1              @ Estado para saber se está em trânsito ou não
+stop_name_address: .skip 4          @ Variável para guardar o endreço do nome da próxima parada
 
 .equ STDOUT, 1
 .equ WRITE_FLAG, 4                  @ Flags para configurar saída no console
