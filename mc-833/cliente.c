47a1a05dd2cf0bfb78fc695962ad2475fb9ae7a3
Victor Andrietta Razoli Silva <ra187835@zero.lab.ic.unicamp.br>
2019-10-07 22:25:40 -0300

    reading input file

--- mc-833/cliente.c
+++ mc-833/cliente.c
@@ -16,6 +16,7 @@ int main(int argc, char **argv) {
    int    sockfd, n;
    char   recvline[MAXLINE + 1];
    char   error[MAXLINE + 1];
+   char  input_line[MAXLINE +1];
    struct sockaddr_in servaddr;
    struct sockaddr_in socket_info;
 
@@ -33,6 +34,7 @@ int main(int argc, char **argv) {
       exit(1);
    }
 
+
   //inicializa com 0 a struct que representa o socket servidor.
    bzero(&servaddr, sizeof(servaddr));
    servaddr.sin_family = AF_INET;
@@ -47,6 +49,20 @@ int main(int argc, char **argv) {
       perror("connect error");
       exit(1);
    }
+
+   bzero(input_line, sizeof(inputline));
+   while(1){
+     int args_read = scanf("%[^\n]",input_line);
+     if(args_read == EOF){
+       break;
+     }
+     write(sockfd, input_line, strlen(inputline));
+     for(;;){
+
+     }
+   }
+
+
 
    //enquanto há mensagens enviadas do servidor
    while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
@@ -69,10 +85,10 @@ int main(int argc, char **argv) {
       exit(1);
    }
 
-   //printa as informações do socket do cliente obtidas
-   printf("\nSocket Family %u", socket_info.sin_family);
-   printf("\nSocket Port %u", socket_info.sin_port);
-   printf("\nSocket Address %s\n", inet_ntoa(socket_info.sin_addr));
+   // //printa as informações do socket do cliente obtidas
+   // printf("\nSocket Family %u", socket_info.sin_family);
+   // printf("\nSocket Port %u", socket_info.sin_port);
+   // printf("\nSocket Address %s\n", inet_ntoa(socket_info.sin_addr));
 
    if (n < 0) {
       perror("read error");
