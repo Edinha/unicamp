f0c48aba23272e76526aa22513deb02ba2cbf526
vitor andrietta <vitorandrietta@gmail.com>
2019-10-09 00:46:48 -0300

    slide impl

--- mc-833/cliente.c
+++ mc-833/cliente.c
@@ -1,99 +1,100 @@
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
-#include <netdb.h>
-#include <string.h>
-#include <errno.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #define MAXLINE 4096
+#define CLOSE_CON "closecon"
+
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    }
+
+    return b;
+}
 
 int main(int argc, char **argv) {
-   int    sockfd, n;
-   char   recvline[MAXLINE + 1];
-   char   error[MAXLINE + 1];
-   char  input_line[MAXLINE +1];
-   struct sockaddr_in servaddr;
-   struct sockaddr_in socket_info;
+    typedef enum {
+        false, true
+    } bool;
 
-   if (argc != 2) { // Se IP do server não fornecido
-      strcpy(error,"uso: ");
-      strcat(error,argv[0]);
-      strcat(error," <IPaddress>");
-      perror(error);
-      exit(1); //encerre
-   }
-
-  //cria o socket TCP do cliente associado a conexão
-   if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-      perror("socket error");
-      exit(1);
-   }
+    int sockfd, maxsd;
+    char recvline[MAXLINE + 1];
+    char error[MAXLINE + 1];
+    char input_line[MAXLINE + 1];
+    struct sockaddr_in servaddr;
 
 
-  //inicializa com 0 a struct que representa o socket servidor.
-   bzero(&servaddr, sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_port   = htons(22122);
-   //associa o IP do servidor à conexão
-   if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-      perror("inet_pton error");
-      exit(1);
-   }
-   //performa a conexão
-   if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-      perror("connect error");
-      exit(1);
-   }
+    fd_set readfds;
 
-   bzero(input_line, sizeof(input_line));
-   while(1){
-     int args_read = scanf("%[^\n]",input_line);
-     if(args_read == EOF){
-       break;
-     }
-     write(sockfd, input_line, strlen(input_line));
-     for(;;){
+    if (argc != 2) { // Se IP do server não fornecido
+        strcpy(error, "uso: ");
+        strcat(error, argv[0]);
+        strcat(error, " <IPaddress>");
+        perror(error);
+        exit(1); //encerre
+    }
 
-     }
-   }
+    //cria o socket TCP do cliente associado a conexão
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket error");
+        exit(1);
+    }
 
 
+    //inicializa com 0 a struct que representa o socket servidor.
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(22122);
+    //associa o IP do servidor à conexão
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+        perror("inet_pton error");
+        exit(1);
+    }
+    //performa a conexão
+    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        perror("connect error");
+        exit(1);
+    }
 
-   //enquanto há mensagens enviadas do servidor
-   while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
-      //coloca um final para a string
-      recvline[n] = 0;
-      //escreve no saída padrão a mensagem recebida
-      if (fputs(recvline, stdout) == EOF) {
-         perror("fputs error");
-         exit(1);
-      }
-   }
+    bzero(input_line, sizeof(input_line));
+    bool end_read = false;
+    FD_ZERO(&readfds);
 
-   //inicializa com 0 a struct que representa a informação do socket local
-   bzero(&socket_info, sizeof(socket_info));
-   // associa a len o tamanho da struct que descreve o socket
-   int len = sizeof(socket_info);
-   //recupera as informações do socket do cliente
-   if (getsockname(sockfd, (struct sockaddr *) &socket_info, (socklen_t *) &len) < 0) {
-      perror("getsockname error");
-      exit(1);
-   }
+    while (true) {
+        FD_SET(fileno(stdin), &readfds);
+        FD_SET(sockfd, &readfds);
+        maxsd = max(fileno(stdin), sockfd) + 1;
+        select(maxsd, &readfds, NULL, NULL, NULL);
 
-   // //printa as informações do socket do cliente obtidas
-   // printf("\nSocket Family %u", socket_info.sin_family);
-   // printf("\nSocket Port %u", socket_info.sin_port);
-   // printf("\nSocket Address %s\n", inet_ntoa(socket_info.sin_addr));
+        if (FD_ISSET(sockfd, &readfds)) {
+            read(sockfd, recvline, MAXLINE);
+            if (strcmp(recvline, CLOSE_CON) == 0) {
+                if (!end_read) {
+                    perror("server terminated suddenly");
+                } else {
+                    break;
+                }
+            } else {
+                fputs(recvline, stdout);
+            }
+        }
 
-   if (n < 0) {
-      perror("read error");
-      exit(1);
-   }
+        if (FD_ISSET(fileno(stdin), &readfds)) {
+            int args_read = scanf("%[^\n]", input_line);
+            if (args_read == EOF) {
+                end_read = true;
+                strcpy(input_line, CLOSE_CON);
+            }
+            write(sockfd, input_line, strlen(input_line));
+        }
+    }
 
-   exit(0);
+    close(sockfd);
+    exit(0);
 }
--- mc-833/servidor.c
+++ mc-833/servidor.c
@@ -1,80 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
-#include <netdb.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <time.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
+#define MAXLINE 4096
 #define LISTENQ 10
 #define MAXDATASIZE 100
+#define CLOSE_CON "closecon"
 
-int main (int argc, char **argv) {
-   int    listenfd, connfd;
-   struct sockaddr_in servaddr;
-   char   buf[MAXDATASIZE];
-   time_t ticks;
-   struct sockaddr_in socket_info;
-
-   //cria o socket TCP associado ao servidor
-   if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-      perror("socket");
-      exit(1);
-   }
-
-   //inicializa com zero os campos da struct associada ao socket do servidor.
-   bzero(&servaddr, sizeof(servaddr));
-   //Comunicação IPV4
-   servaddr.sin_family      = AF_INET;
-   //Receber a conexão de clientes rodandno em qualquer porta
-   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-   //seta a porta associada ao processo do servidor
-   servaddr.sin_port        = htons(22122);
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    struct sockaddr_in servaddr;
+    char buf[MAXDATASIZE];
 
 
-   //associa a conexão às respectivas portas e IPs descritos na estrutura
-   if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
-      perror("bind");
-      exit(1);
-   }
+    char recvline[MAXLINE + 1];
+    //cria o socket TCP associado ao servidor
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
 
-   //verifica se o socket não está ouvindo
-   if (listen(listenfd, LISTENQ) == -1) {
-      perror("listen");
-      exit(1);
-   }
+    //inicializa com zero os campos da struct associada ao socket do servidor.
+    bzero(&servaddr, sizeof(servaddr));
+    //Comunicação IPV4
+    servaddr.sin_family = AF_INET;
+    //Receber a conexão de clientes rodandno em qualquer porta
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    //seta a porta associada ao processo do servidor
+    servaddr.sin_port = htons(22122);
 
-   for ( ; ; ) {
-     /*erro ao obter os dados e retirar da fila a conexão concluída*/
-      if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
-         perror("accept");
-         exit(1);
-      }
 
-      //printa as informações de data e hora
-      ticks = time(NULL);
-      snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks));
-      write(connfd, buf, strlen(buf));
-      
-      //inicializa a estrutura que será usada para obter informações da conexão
-      bzero(&socket_info, sizeof(socket_info));
-      int len = sizeof(socket_info);
-      //recupera as informações do socket do cliente na conexão
-      if (getpeername(connfd, (struct sockaddr *) &socket_info, (socklen_t *) &len) < 0) {
-         perror("getsockname error");
-         exit(1);
-      }
+    //associa a conexão às respectivas portas e IPs descritos na estrutura
+    if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
+        perror("bind");
+        exit(1);
+    }
 
-      //imprime as informações do socket do cliente
-      printf("\nClient Socket Family %u", socket_info.sin_family);
-      printf("\nClient Socket Port %u", socket_info.sin_port);
-      printf("\nClient Socket Address %s\n", inet_ntoa(socket_info.sin_addr));
+    //verifica se o socket não está ouvindo
+    if (listen(listenfd, LISTENQ) == -1) {
+        perror("listen");
+        exit(1);
+    }
 
-      close(connfd);
-   }
-   return(0);
+    for (;;) {
+        /*erro ao obter os dados e retirar da fila a conexão concluída*/
+        if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1) {
+            perror("accept");
+            exit(1);
+        }
+
+        //printa as informações de data e hora
+//      ticks = time(NULL);
+//      snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks));
+
+        while (read(connfd, recvline, MAXLINE) > 0) {
+            strcpy(buf, recvline);
+            write(connfd, buf, strlen(buf));
+            if (strcmp(CLOSE_CON, recvline) == 0) {
+                break;
+            }
+        }
+
+
+        //inicializa a estrutura que será usada para obter informações da conexão
+//      bzero(&socket_info, sizeof(socket_info));
+//      int len = sizeof(socket_info);
+//      //recupera as informações do socket do cliente na conexão
+//      if (getpeername(connfd, (struct sockaddr *) &socket_info, (socklen_t *) &len) < 0) {
+//         perror("getsockname error");
+//         exit(1);
+//      }
+
+        //imprime as informações do socket do cliente
+//      printf("\nClient Socket Family %u", socket_info.sin_family);
+//      printf("\nClient Socket Port %u", socket_info.sin_port);
+//      printf("\nClient Socket Address %s\n", inet_ntoa(socket_info.sin_addr));
+
+        close(connfd);
+        break;
+    }
+    return (0);
 }
