f7ac9fe529762361dbd212ee6e048fd0d9b70c28
edinha <edinha.gon.cruz@gmail.com>
2020-03-26 08:42:24 -0300

    T1 - resolution fix

--- mc-920/t1/resolution.py
+++ mc-920/t1/resolution.py
@@ -3,12 +3,18 @@ import numpy
 
 class Resolution(Image):
 
-    def resolution_cut_half(self):
+    def resolution_cut_half(self, step):
         img = self.get()
-        img = img[::2]
-        img = img.T[::2].T
 
-        self.set(img)
+        for i in range(1, step):
+            img[i::step] = img[::step]
+
+        img = img.T
+
+        for i in range(1, step):
+            img[i::step] = img[::step]
+
+        self.set(img.T)
 
 if __name__ == "__main__":
     import sys
@@ -16,6 +22,7 @@ if __name__ == "__main__":
     (w, h) = r.get().shape
     i = w // 2
     while i >= 8:
-        r.resolution_cut_half()
+        res = w // i
+        r.resolution_cut_half(res)
         r.save_to_file("output-{}.png".format(i))
         i = i // 2
