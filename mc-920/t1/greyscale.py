14a19a2facef97169e36172dff14924573f6bd00
edinha <edinha.gon.cruz@gmail.com>
2020-03-30 09:27:55 -0300

    T1 - greyscale changes

--- mc-920/t1/greyscale.py
+++ mc-920/t1/greyscale.py
@@ -6,9 +6,10 @@ MAX_GREY_SCALE = 256
 
 class Greyscale(Image):
 
-    def log(self, c, _):
+    def log(self, _):
+        c = self.get_log_transform_constant()
         self.apply_to_all_pixels(self.apply_log(c))
-        self.apply_to_all_pixels(self.normalize)
+        self.astype(numpy.uint8)
 
     def apply_log(self, c):
         def g(pixel):
@@ -16,8 +17,12 @@ class Greyscale(Image):
 
         return g
 
-    def exp(self, c, e):
+    def exp(self, e):
+        e = float(e)
+        c = self.get_exp_transform_constant()
+        self.apply_to_all_pixels(self.transform_scale)
         self.apply_to_all_pixels(self.apply_exp(c, e))
+        self.apply_to_all_pixels(self.return_scale)
         self.apply_to_all_pixels(self.normalize)
 
     def apply_exp(self, c, e):
@@ -26,8 +31,11 @@ class Greyscale(Image):
 
         return g
 
-    def square(self, c, _):
+    def square(self, _):
+        c = self.get_square_constant()
+        self.apply_to_all_pixels(self.transform_scale)
         self.apply_to_all_pixels(self.apply_square(c))
+        self.apply_to_all_pixels(self.return_scale)
         self.apply_to_all_pixels(self.normalize)
 
     def apply_square(self, c):
@@ -36,7 +44,8 @@ class Greyscale(Image):
 
         return g
 
-    def sqrt(self, c, _):
+    def sqrt(self, _):
+        c = self.get_sqrt_constant()
         self.apply_to_all_pixels(self.apply_sqrt(c))
         self.apply_to_all_pixels(self.normalize)
 
@@ -46,11 +55,33 @@ class Greyscale(Image):
 
         return g
 
-    def normalize(self, pixel):
-        return numpy.uint8(pixel % MAX_GREY_SCALE)
+    def transform_scale(self, pixel):
+        return pixel / 255.0
+
+    def return_scale(self, pixel):
+        return numpy.uint8(pixel * 255)
+
+    def normalize(self, img):
+        img[img < 0] = 0
+        img[img > 255] = 255
+        return numpy.uint8(img)
+
+    def get_log_transform_constant(self):
+        img = self.get()
+        return MAX_GREY_SCALE / (numpy.log(1 + numpy.amax(img)))
+
+    def get_exp_transform_constant(self):
+        return 20.2
+
+    def get_square_constant(self):
+        return 1
+
+    def get_sqrt_constant(self):
+        return 10
 
 if __name__ == "__main__":
     import sys
     i = Greyscale(sys.argv[1])
-    getattr(i, sys.argv[2])(int(sys.argv[3]), float(sys.argv[4]))
+    l = len(sys.argv) - 1
+    getattr(i, sys.argv[2])(sys.argv[min(3, l)])
     i.save_to_file("output.png")
