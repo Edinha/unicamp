9e3b1cc5b648cf0bea3af6df02b84d9bb958dd3b
edinha <edinha.gon.cruz@gmail.com>
2020-04-16 15:29:58 -0300

    T2 - only using numpy array

--- mc-920/t2/convolution.py
+++ mc-920/t2/convolution.py
@@ -4,12 +4,11 @@ from scipy.ndimage import convolve
 
 class FilterConstants():
     h3 = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
-    h4 = [[-1, 2, 1], [0, 0, 0], [1, 2, 1]]
+    h4 = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
     h5 = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]
     h6 = [[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]]
     h7 = [[-1, -1, 2], [-1, 2, -1], [2, -1, -1]]
     h8 = [[2, -1, -1], [-1, 2, -1], [-1, -1, 2]]
-
 
     @staticmethod
     def get_filter_matrix(name):
@@ -30,9 +29,45 @@ class Convolution(Image):
 
         self.set(img)
 
+    def apply_def(self, filter_name):
+        img = self.get()
+        kernel = FilterConstants.get_filter_matrix(filter_name)
+
+        sub_shape = (len(kernel), len(kernel))
+        view_shape = tuple(numpy.subtract(img.shape, sub_shape) + 1) + sub_shape
+        strides = img.strides + img.strides
+
+        sub_matrices = numpy.lib.stride_tricks.as_strided(img, view_shape, strides)
+        sub_matrices = self.multiply_filter(kernel)(sub_matrices)
+
+        img = numpy.array([[numpy.sum(s) for s in a] for a in sub_matrices])
+        self.set(self.normalize(img))
+
+    def multiply_filter(self, kernel):
+        def f(pixel):
+            return pixel * kernel
+
+        return f
+
+    def sum_result(self):
+        def f(pixel):
+            return numpy.sum(pixel)
+
+        return f
+
+    def change_interval(self, pixel):
+        return pixel / 255
+
+    def return_interval(self, pixel):
+        return pixel * 255
+
+    def normalize(self, img):
+        img[img < 0] = 0
+        img[img > 255] = 0
+        return numpy.uint8(img)
 
 if __name__ == "__main__":
     import sys
     i = Convolution(sys.argv[1])
-    i.apply(sys.argv[2])
+    i.apply_def(sys.argv[2])
     i.save_to_file("output.png")
