d65b48bdcfd23d3888df6d0558cc00c92526d7cc
edinha <edinha.gon.cruz@gmail.com>
2020-04-16 14:24:47 -0300

    T2 - scipy convolve working (?)

--- mc-920/t2/convolution.py
+++ mc-920/t2/convolution.py
@@ -1,8 +1,15 @@
 from image import Image
 import numpy
+from scipy.ndimage import convolve
 
 class FilterConstants():
     h3 = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+    h4 = [[-1, 2, 1], [0, 0, 0], [1, 2, 1]]
+    h5 = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]
+    h6 = [[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]]
+    h7 = [[-1, -1, 2], [-1, 2, -1], [2, -1, -1]]
+    h8 = [[2, -1, -1], [-1, 2, -1], [-1, -1, 2]]
+
 
     @staticmethod
     def get_filter_matrix(name):
@@ -11,9 +18,17 @@ class FilterConstants():
 class Convolution(Image):
 
     def apply(self, filter_name):
-        filter = FilterConstants.get_filter_matrix(filter_name)
+        kernel = FilterConstants.get_filter_matrix(filter_name)
 
-        print("FILTER CHOSEN: ", filter)
+        img = self.get()
+        print("FILTER CHOSEN: ", kernel)
+        print("IMAGE STAND: ", img)
+
+        img = convolve(img, kernel, mode='nearest')
+
+        print("NEW IMAGE: ", img)
+
+        self.set(img)
 
 
 if __name__ == "__main__":
