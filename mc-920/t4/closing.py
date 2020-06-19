6daa006a035726798140fc92cfd499cbf8fa3be3
edinha <edinha.gon.cruz@gmail.com>
2020-06-19 09:09:31 -0300

    feat: closing

---
+++ mc-920/t4/closing.py
@@ -0,0 +1,21 @@
+from image import Image
+import numpy
+from erosion import Erosion
+from dilation import Dilation
+from scipy.ndimage.morphology import binary_erosion
+
+class Closing(Erosion, Dilation):
+
+    def close(self, structure):
+        self.dilate(structure)
+        self.erode(structure)
+
+if __name__ == "__main__":
+    import sys
+    i = Closing(sys.argv[1])
+    i.binary_set()
+    structure = numpy.ones((int(sys.argv[2]), int(sys.argv[3])))
+    i.close(structure)
+    i.binary_normalize()
+    print("IMG: ", i.get().max(), i.get())
+    i.save_to_file("closing.pbm")
