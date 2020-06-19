7926dcc64dcf8f29753aff4a73e8268fecc05717
edinha <edinha.gon.cruz@gmail.com>
2020-06-19 08:30:25 -0300

    feat: dilation and erosion params

--- mc-920/t4/dilation.py
+++ mc-920/t4/dilation.py
@@ -1,12 +1,12 @@
 from image import Image
 import numpy
-from scipy.ndimage.morphology import binary_erosion
+from scipy.ndimage.morphology import binary_dilation
 
-class Erosion(Image):
+class Dilation(Image):
 
-    def erode(self, structure):
+    def dilate(self, structure):
         img = self.get()
-        result = binary_erosion(img, structure=structure)
+        result = binary_dilation(img, structure=structure)
         self.set(result)
         self.astype(img.dtype)
 
@@ -18,10 +18,10 @@ class Erosion(Image):
 
 if __name__ == "__main__":
     import sys
-    i = Erosion(sys.argv[1])
+    i = Dilation(sys.argv[1])
     i.binary_set()
     structure = numpy.ones((int(sys.argv[2]), int(sys.argv[3])))
-    i.erode(structure)
+    i.dilate(structure)
     i.normalize()
     print("IMG: ", i.get().max(), i.get())
-    i.save_to_file("erosion.pbm")
+    i.save_to_file("dilation.pbm")
